00c5ef9fd1c6bc962464219a0af9526117728869
drinwat <varavshenrijs@gmail.com>
2023-05-23 18:49:46 +0300

    Pievienoju Programma01file

---
+++ Programma01/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Programma01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.butbalts = new System.Windows.Forms.Button();
+            this.butmelns = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(124, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mainīt fona krāsu";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // butbalts
+            // 
+            this.butbalts.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butbalts.ForeColor = System.Drawing.Color.Red;
+            this.butbalts.Location = new System.Drawing.Point(82, 155);
+            this.butbalts.Name = "butbalts";
+            this.butbalts.Size = new System.Drawing.Size(93, 50);
+            this.butbalts.TabIndex = 2;
+            this.butbalts.Text = "BALTS";
+            this.butbalts.UseVisualStyleBackColor = true;
+            this.butbalts.Click += new System.EventHandler(this.butbalts_Click);
+            // 
+            // butmelns
+            // 
+            this.butmelns.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butmelns.ForeColor = System.Drawing.Color.Red;
+            this.butmelns.Location = new System.Drawing.Point(305, 155);
+            this.butmelns.Name = "butmelns";
+            this.butmelns.Size = new System.Drawing.Size(93, 50);
+            this.butmelns.TabIndex = 3;
+            this.butmelns.Text = "MELNS";
+            this.butmelns.UseVisualStyleBackColor = true;
+            this.butmelns.Click += new System.EventHandler(this.butmelns_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.butmelns);
+            this.Controls.Add(this.butbalts);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Melnbalts";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button butbalts;
+        private System.Windows.Forms.Button butmelns;
+    }
+}
+
---
+++ Programma01/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Programma01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butbalts_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+        }
+
+        private void butmelns_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+        }
+    }
+}
