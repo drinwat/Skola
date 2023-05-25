8d9852be261c6cac3feb7d21393aa7ffd03cb23e
drinwat <varavshenrijs@gmail.com>
2023-05-25 18:03:08 +0300

    step1
    step1

--- Personalitates test02.Designer.Designer.cs
+++ Personalitates test02.Designer.Designer.cs
@@ -29,20 +29,56 @@ namespace mbti02
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.butsakt = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(279, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(657, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vai vēlies uzzināt savu MBTI tipu?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // butsakt
+            // 
+            this.butsakt.BackColor = System.Drawing.Color.Silver;
+            this.butsakt.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.butsakt.ForeColor = System.Drawing.Color.Red;
+            this.butsakt.Location = new System.Drawing.Point(362, 466);
+            this.butsakt.Name = "butsakt";
+            this.butsakt.Size = new System.Drawing.Size(488, 49);
+            this.butsakt.TabIndex = 1;
+            this.butsakt.Text = "Spied šeit, lai noskaidrotu!";
+            this.butsakt.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::mbti02.Properties.Resources.MBTIpic;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1212, 553);
+            this.Controls.Add(this.butsakt);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "test02";
+            this.Text = "shortMBTItest";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button butsakt;
     }
 }
 
