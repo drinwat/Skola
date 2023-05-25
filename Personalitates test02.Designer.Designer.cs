385052c15d63ef3bf89cbaaa99e00a8665065191
drinwat <varavshenrijs@gmail.com>
2023-05-25 17:03:49 +0300

    testtest
    haha

--- Personalitates test02.Designer.Designer.cs
+++ Personalitates test02.Designer.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace personalitates_tests01
+namespace mbti02
 {
     partial class Form1
     {
@@ -29,10 +29,17 @@ namespace personalitates_tests01
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.Text = "test02";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Personalitates test02.Designer.cs
+++ Personalitates test02.Designer.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace personalitates_tests01
+namespace mbti02
 {
     public partial class Form1 : Form
     {
