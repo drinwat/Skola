0ec65d55f253c1a81811cdcefb2eb655f0a6cebb
drinwat <varavshenrijs@gmail.com>
2023-05-28 19:18:52 +0300

    dD
    DVd

---
+++ MD/WindowsFormsApp111/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Iegūstam pašreizējo datumu un laiku
+        DateTime currentTime = DateTime.Now;
+
+        // Saglabājam datumu un laiku failā
+        string filePath = "log.txt";
+        string logEntry = $"Programma iedarbināta: {currentTime}";
+        File.AppendAllText(filePath, logEntry + Environment.NewLine);
+
+        // Paziņojums, ka dati ir saglabāti
+        Console.WriteLine("Dati ir saglabāti failā.");
+
+        // Pauze, lai redzētu rezultātu
+        Console.ReadLine();
+    }
+}
+
+
