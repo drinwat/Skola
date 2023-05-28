37102aa3d3c124569f2c886c5edb1766c0ef65f7
drinwat <varavshenrijs@gmail.com>
2023-05-28 18:06:33 +0300

    wrvb
    wav

--- MD/Blackwhite/Krāsains ekrāns/Form1.Designer.cs
+++ MD/Blackwhite/Krāsains ekrāns/Form1.Designer.cs
@@ -125,7 +125,7 @@ namespace Blackwhite
             this.Controls.Add(this.butsarkans);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Krāsains ekrāns";
+            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
---
+++ MD/Blackwhite/Krāsains ekrāns/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Blackwhite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butzils_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+        }
+
+        private void butroza_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.MistyRose;
+        }
+
+        private void butdzeltens_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Yellow;
+        }
+
+        private void butzals_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+    }
+}
