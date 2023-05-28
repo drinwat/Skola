76608987511d751a90ec381182905e99413e61f9
drinwat <varavshenrijs@gmail.com>
2023-05-28 19:19:56 +0300

    ad
    ad

--- MD/WindowsFormsApp111/Program.cs
+++ MD/WindowsFormsApp111/Program.cs
@@ -5,18 +5,15 @@ class Program
 {
     static void Main()
     {
-        // Iegūstam pašreizējo datumu un laiku
+       
         DateTime currentTime = DateTime.Now;
 
-        // Saglabājam datumu un laiku failā
         string filePath = "log.txt";
         string logEntry = $"Programma iedarbināta: {currentTime}";
         File.AppendAllText(filePath, logEntry + Environment.NewLine);
 
-        // Paziņojums, ka dati ir saglabāti
         Console.WriteLine("Dati ir saglabāti failā.");
 
-        // Pauze, lai redzētu rezultātu
         Console.ReadLine();
     }
 }
