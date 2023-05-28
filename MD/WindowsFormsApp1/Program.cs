7b2beaf7e403a4f2423ea2b85aa34eee6f4dca34
drinwat <133667924+drinwat@users.noreply.github.com>
2023-05-28 19:02:17 +0300

    Update Program.cs

--- MD/WindowsFormsApp1/Program.cs
+++ MD/WindowsFormsApp1/Program.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System;
+using System.IO;
 
-namespace WindowsFormsApp1
+class Program
 {
-    static class Program
+    static void Main()
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+      
+        DateTime currentTime = DateTime.Now;
+
+        string filePath = "log.txt";
+        string logEntry = $"Programma iedarbināta: {currentTime}";
+        File.AppendAllText(filePath, logEntry + Environment.NewLine);
+
+        Console.WriteLine("Dati ir saglabāti failā.");
+
+        Console.ReadLine();
     }
 }
+
